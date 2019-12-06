dfb8435ec8aa69a6c9a301421e493754cafd18b0
Vinkhey <50228137+Vinkhey@users.noreply.github.com>
2019-12-06 16:01:33 +0100

    Update Login.cs

--- CardGame/CardGameInterface/Login.cs
+++ CardGame/CardGameInterface/Login.cs
@@ -55,6 +55,29 @@ namespace CardGameInterface
         {
             this.MaximumSize = new Size(1280, 800);
             this.MinimumSize = new Size(1280, 800);
+<<<<<<< Updated upstream
+=======
+
+            this.WindowState = Properties.Settings.Default.F1State;
+            this.Location = Properties.Settings.Default.F1Location;
+            this.Size = Properties.Settings.Default.F1Size;
+
+            if (Properties.Settings.Default.F1Size.Width == 0) Properties.Settings.Default.Upgrade();
+
+            if (Properties.Settings.Default.F1Size.Width == 0 || Properties.Settings.Default.F1Size.Height == 0)
+            {
+
+            }
+            else
+            {
+                this.WindowState = Properties.Settings.Default.F1State;
+
+                if (this.WindowState == FormWindowState.Minimized) this.WindowState = FormWindowState.Normal;
+
+                this.Location = Properties.Settings.Default.F1Location;
+                this.Size = Properties.Settings.Default.F1Size;
+            }
+>>>>>>> Stashed changes
         }
 
         private void BtnLogin_Click(object sender, EventArgs e)
@@ -117,5 +140,28 @@ namespace CardGameInterface
             this.Hide();
             SignUpForm.ShowDialog();
         }
+<<<<<<< Updated upstream
+=======
+
+        private void Login_Closing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.F1State = this.WindowState;
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                // save location and size if the state is normal
+                Properties.Settings.Default.F1Location = this.Location;
+                Properties.Settings.Default.F1Size = this.Size;
+            }
+            else
+            {
+                // save the RestoreBounds if the form is minimized or maximized!
+                Properties.Settings.Default.F1Location = this.RestoreBounds.Location;
+                Properties.Settings.Default.F1Size = this.RestoreBounds.Size;
+            }
+
+            // don't forget to save the settings
+            Properties.Settings.Default.Save();
+        }
+>>>>>>> Stashed changes
     }
 }
