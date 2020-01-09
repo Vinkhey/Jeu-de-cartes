2f82707495efcde3c85bb0b1036018bcde68893d
EVANGELISTI Bryan <Bryan.EVANGELISTI@cpnv.ch>
2020-01-09 14:55:26 +0100

    Interface du plateau
    Ajout de l'onglet tas de carte

--- CardGame/CardGameInterface/CardGameMainInterface.Designer.cs
+++ CardGame/CardGameInterface/CardGameMainInterface.Designer.cs
@@ -32,27 +32,40 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.BtnQuitGame = new System.Windows.Forms.Button();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.button8 = new System.Windows.Forms.Button();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.tabControl2 = new System.Windows.Forms.TabControl();
             this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.button9 = new System.Windows.Forms.Button();
             this.tabPage4 = new System.Windows.Forms.TabPage();
             this.button7 = new System.Windows.Forms.Button();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button11 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.progressBar2 = new System.Windows.Forms.ProgressBar();
-            this.BtnQuitGame = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
+            this.tabPage6.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.tabControl2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
             this.tabPage4.SuspendLayout();
+            this.tabPage5.SuspendLayout();
             this.SuspendLayout();
             // 
             // progressBar1
@@ -79,6 +92,7 @@
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage6);
             this.tabControl1.Location = new System.Drawing.Point(708, 36);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
@@ -87,6 +101,7 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.BtnQuitGame);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
@@ -94,6 +109,16 @@
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Menu";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // BtnQuitGame
+            // 
+            this.BtnQuitGame.Location = new System.Drawing.Point(292, 185);
+            this.BtnQuitGame.Name = "BtnQuitGame";
+            this.BtnQuitGame.Size = new System.Drawing.Size(101, 23);
+            this.BtnQuitGame.TabIndex = 4;
+            this.BtnQuitGame.Text = "Quitter la partie";
+            this.BtnQuitGame.UseVisualStyleBackColor = true;
+            this.BtnQuitGame.Click += new System.EventHandler(this.BtnQuitGame_Click);
             // 
             // tabPage2
             // 
@@ -114,6 +139,49 @@
             this.button8.Size = new System.Drawing.Size(110, 161);
             this.button8.TabIndex = 1;
             this.button8.UseVisualStyleBackColor = false;
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.Controls.Add(this.button10);
+            this.tabPage6.Controls.Add(this.label2);
+            this.tabPage6.Controls.Add(this.label1);
+            this.tabPage6.Location = new System.Drawing.Point(4, 22);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage6.Size = new System.Drawing.Size(399, 214);
+            this.tabPage6.TabIndex = 2;
+            this.tabPage6.Text = "tas de cartes";
+            this.tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.Location = new System.Drawing.Point(19, 149);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(144, 47);
+            this.button10.TabIndex = 2;
+            this.button10.Text = "Ajouter une carte";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(147, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "...";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cartes restantes :";
             // 
             // button3
             // 
@@ -161,6 +229,7 @@
             // 
             this.tabControl2.Controls.Add(this.tabPage3);
             this.tabControl2.Controls.Add(this.tabPage4);
+            this.tabControl2.Controls.Add(this.tabPage5);
             this.tabControl2.Location = new System.Drawing.Point(708, 31);
             this.tabControl2.Name = "tabControl2";
             this.tabControl2.SelectedIndex = 0;
@@ -169,6 +238,7 @@
             // 
             // tabPage3
             // 
+            this.tabPage3.Controls.Add(this.button9);
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
@@ -176,6 +246,15 @@
             this.tabPage3.TabIndex = 0;
             this.tabPage3.Text = "Menu";
             this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(292, 185);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(101, 23);
+            this.button9.TabIndex = 5;
+            this.button9.Text = "Quitter la partie";
+            this.button9.UseVisualStyleBackColor = true;
             // 
             // tabPage4
             // 
@@ -197,6 +276,49 @@
             this.button7.Size = new System.Drawing.Size(110, 161);
             this.button7.TabIndex = 0;
             this.button7.UseVisualStyleBackColor = false;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Controls.Add(this.label4);
+            this.tabPage5.Controls.Add(this.label3);
+            this.tabPage5.Controls.Add(this.button11);
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(399, 214);
+            this.tabPage5.TabIndex = 2;
+            this.tabPage5.Text = "tas de cartes";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(147, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "...";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Cartes restantes :";
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.Location = new System.Drawing.Point(10, 150);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(144, 47);
+            this.button11.TabIndex = 3;
+            this.button11.Text = "Ajouter une carte";
+            this.button11.UseVisualStyleBackColor = true;
             // 
             // button6
             // 
@@ -234,23 +356,12 @@
             this.progressBar2.Size = new System.Drawing.Size(585, 31);
             this.progressBar2.TabIndex = 3;
             // 
-            // BtnQuitGame
-            // 
-            this.BtnQuitGame.Location = new System.Drawing.Point(1078, 391);
-            this.BtnQuitGame.Name = "BtnQuitGame";
-            this.BtnQuitGame.Size = new System.Drawing.Size(101, 23);
-            this.BtnQuitGame.TabIndex = 4;
-            this.BtnQuitGame.Text = "Quitter la partie";
-            this.BtnQuitGame.UseVisualStyleBackColor = true;
-            this.BtnQuitGame.Click += new System.EventHandler(this.BtnQuitGame_Click);
-            // 
             // CardGameMainInterface
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkGray;
             this.ClientSize = new System.Drawing.Size(1215, 740);
-            this.Controls.Add(this.BtnQuitGame);
             this.Controls.Add(this.progressBar2);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
@@ -262,10 +373,16 @@
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.CardGameMainInterface_FormClosing);
             this.groupBox1.ResumeLayout(false);
             this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
             this.tabPage2.ResumeLayout(false);
+            this.tabPage6.ResumeLayout(false);
+            this.tabPage6.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.tabControl2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
             this.tabPage4.ResumeLayout(false);
+            this.tabPage5.ResumeLayout(false);
+            this.tabPage5.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -291,5 +408,14 @@
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button BtnQuitGame;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button11;
     }
 }
