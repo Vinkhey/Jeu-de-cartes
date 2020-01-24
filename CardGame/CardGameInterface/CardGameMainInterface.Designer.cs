072c2c5f9415a0a436f7dcd51ba6444f688a2695
EVANGELISTI Bryan <Bryan.EVANGELISTI@cpnv.ch>
2020-01-24 08:39:30 +0100

    Centrage d'image
    oui

--- CardGame/CardGameInterface/CardGameMainInterface.Designer.cs
+++ CardGame/CardGameInterface/CardGameMainInterface.Designer.cs
@@ -48,21 +48,18 @@
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.button3);
             this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Location = new System.Drawing.Point(38, 646);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Location = new System.Drawing.Point(25, 420);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.groupBox1.Size = new System.Drawing.Size(1731, 460);
+            this.groupBox1.Size = new System.Drawing.Size(1154, 299);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Joueur 1";
             // 
             // BtnNewCardPlayer1
             // 
-            this.BtnNewCardPlayer1.Location = new System.Drawing.Point(1275, 372);
-            this.BtnNewCardPlayer1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.BtnNewCardPlayer1.Location = new System.Drawing.Point(850, 242);
             this.BtnNewCardPlayer1.Name = "BtnNewCardPlayer1";
-            this.BtnNewCardPlayer1.Size = new System.Drawing.Size(195, 52);
+            this.BtnNewCardPlayer1.Size = new System.Drawing.Size(130, 34);
             this.BtnNewCardPlayer1.TabIndex = 9;
             this.BtnNewCardPlayer1.Text = "Tirer une carte";
             this.BtnNewCardPlayer1.UseVisualStyleBackColor = true;
@@ -70,10 +67,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1504, 372);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Location = new System.Drawing.Point(1003, 242);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(195, 52);
+            this.button1.Size = new System.Drawing.Size(130, 34);
             this.button1.TabIndex = 7;
             this.button1.Text = "Quitter la partie";
             this.button1.UseVisualStyleBackColor = true;
@@ -81,21 +77,18 @@
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.White;
-            this.button3.Location = new System.Drawing.Point(957, 55);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button3.Location = new System.Drawing.Point(638, 36);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(268, 369);
+            this.button3.Size = new System.Drawing.Size(179, 240);
             this.button3.TabIndex = 2;
             this.button3.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.White;
-            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopRight;
-            this.button2.Location = new System.Drawing.Point(396, 55);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Location = new System.Drawing.Point(264, 36);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(268, 369);
+            this.button2.Size = new System.Drawing.Size(179, 240);
             this.button2.TabIndex = 1;
             this.button2.UseVisualStyleBackColor = false;
             // 
@@ -105,31 +98,27 @@
             this.groupBox2.Controls.Add(this.BtnNewCardPlayer2);
             this.groupBox2.Controls.Add(this.button6);
             this.groupBox2.Controls.Add(this.button5);
-            this.groupBox2.Location = new System.Drawing.Point(38, 18);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox2.Location = new System.Drawing.Point(25, 12);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.groupBox2.Size = new System.Drawing.Size(1731, 460);
+            this.groupBox2.Size = new System.Drawing.Size(1154, 299);
             this.groupBox2.TabIndex = 2;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Joueur 2";
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(1504, 377);
-            this.button7.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button7.Location = new System.Drawing.Point(1003, 245);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(195, 52);
+            this.button7.Size = new System.Drawing.Size(130, 34);
             this.button7.TabIndex = 8;
             this.button7.Text = "Quitter la partie";
             this.button7.UseVisualStyleBackColor = true;
             // 
             // BtnNewCardPlayer2
             // 
-            this.BtnNewCardPlayer2.Location = new System.Drawing.Point(1275, 377);
-            this.BtnNewCardPlayer2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.BtnNewCardPlayer2.Location = new System.Drawing.Point(850, 245);
             this.BtnNewCardPlayer2.Name = "BtnNewCardPlayer2";
-            this.BtnNewCardPlayer2.Size = new System.Drawing.Size(195, 52);
+            this.BtnNewCardPlayer2.Size = new System.Drawing.Size(130, 34);
             this.BtnNewCardPlayer2.TabIndex = 8;
             this.BtnNewCardPlayer2.Text = "Tirer une carte";
             this.BtnNewCardPlayer2.UseVisualStyleBackColor = true;
@@ -138,33 +127,30 @@
             // button6
             // 
             this.button6.BackColor = System.Drawing.Color.White;
-            this.button6.Location = new System.Drawing.Point(957, 48);
-            this.button6.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button6.Location = new System.Drawing.Point(638, 31);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(268, 369);
+            this.button6.Size = new System.Drawing.Size(179, 240);
             this.button6.TabIndex = 6;
             this.button6.UseVisualStyleBackColor = false;
             // 
             // button5
             // 
             this.button5.BackColor = System.Drawing.Color.White;
-            this.button5.Location = new System.Drawing.Point(396, 48);
-            this.button5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button5.Location = new System.Drawing.Point(264, 31);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(268, 369);
+            this.button5.Size = new System.Drawing.Size(179, 240);
             this.button5.TabIndex = 5;
             this.button5.UseVisualStyleBackColor = false;
             // 
             // CardGameMainInterface
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkGray;
-            this.ClientSize = new System.Drawing.Size(1822, 1138);
+            this.ClientSize = new System.Drawing.Size(1215, 757);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "CardGameMainInterface";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "CardGameInterface";
